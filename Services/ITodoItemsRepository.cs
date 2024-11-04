c2647cb724a567f1e5515e68206a06ecee459b2e
zjiang10 <zjiang10@slb.com>
2024-11-04 19:44:01 +0800

    feat: copy TodoItemV2Service

---
+++ Services/ITodoItemsRepository.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+using ToDoList.Api.Models;
+
+namespace ToDoList.Api.Services
+{
+    public interface ITodoItemsRepository
+    {
+
+        public Task<List<ToDoItemV2Obj>> FindAllTodoItemsInOneDay(DateTime dateTime);
+        public Task<ToDoItemV2Obj> FindById(string id);
+        public Task<UpdateResult> Save(ToDoItemV2Obj todoItem);
+        public Task<ToDoItemV2Obj> CreateAsync(ToDoItemV2Obj toDoItem);
+        public Task<ToDoItemV2Obj> EditItem(ToDoItemV2Obj item);
+    }
+}
---
+++ Services/TodoItemV2Service.cs
@@ -0,0 +1,50 @@
+﻿using ToDoList.Api.Models;
+
+namespace ToDoList.Api.Services
+{
+    public class TodoItemV2Service
+    {
+        private const int MAX_EDIT_TIMES = 2;
+        private const int MAX_DUEDATE = 8;
+        private readonly ITodoItemsRepository _todosRepository;
+
+        public TodoItemV2Service(ITodoItemsRepository repository)
+        {
+            _todosRepository = repository;
+        }
+
+        public async Task<ToDoItemV2Obj> CreateToDoItem(ToDoItemV2Obj inputToDoItem)
+        {
+
+            return await _todosRepository.CreateAsync(inputToDoItem);
+        }
+
+        public async Task<ToDoItemV2Obj> EditToDoItem(ToDoItemV2Obj inputToDoItem)
+        {
+            String id = inputToDoItem.Id;
+            var existingItem = await _todosRepository.FindById(id);
+            if (existingItem == null)
+            {
+                throw new KeyNotFoundException("Todo item not found.");
+            }
+
+            existingItem.Description = inputToDoItem.Description;
+            existingItem.Favorite = inputToDoItem.Favorite;
+            existingItem.DueDate = inputToDoItem.DueDate;
+
+            return await _todosRepository.EditItem(existingItem);
+        }
+
+        public async Task<ToDoItemV2Obj> GetToDoItemById(string id)
+        {
+            return await _todosRepository.FindById(id);
+        }
+
+        public async Task<List<ToDoItemV2Obj>> GetAllToDoItemsInOneDay(DateTime date)
+        {
+            return await _todosRepository.FindAllTodoItemsInOneDay(date);
+        }
+
+
+    }
+}
